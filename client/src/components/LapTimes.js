f4c3525656644e6c695efc0ec24a4b9cb4c83b0e
KurenaiEXE <sparky00137@hotmail.com>
2023-08-12 10:13:36 +1000

    added zoom

--- client/src/components/LapTimes.js
+++ client/src/components/LapTimes.js
@@ -3,7 +3,7 @@ import { GET_LAP_HISTORY } from '../graphql/queries';
 import { useLazyQuery } from '@apollo/client';
 import { TEAMS } from '../constants';
 import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
-import { Button } from '@mui/material';
+import { Button, Slider } from '@mui/material';
 
 export default function LapTimes({ session_uid }) {
   const [laps, setLaps] = useState([]);
@@ -12,6 +12,21 @@ export default function LapTimes({ session_uid }) {
   const [numTicks, setNumTicks] = useState(5);
   const [opacity, setOpacity] = useState(null);
   const [showDriver, setShowDriver] = useState(null);
+  const [domain, setDomain] = useState([0, 0]);
+  const [dataMin, setDataMin] = useState(-1);
+  const [dataMax, setDataMax] = useState(-1);
+  const [ticks, setTicks] = useState([]);
+
+  // const getAxisYDomain = (from, to, ref, offset) => {
+  //   const refData = initialData.slice(from - 1, to);
+  //   let [bottom, top] = [refData[0][ref], refData[0][ref]];
+  //   refData.forEach((d) => {
+  //     if (d[ref] > top) top = d[ref];
+  //     if (d[ref] < bottom) bottom = d[ref];
+  //   });
+  
+  //   return [(bottom | 0) - offset, (top | 0) + offset];
+  // };
 
   const sortLaps = (a, b) => {
     if (a.lap_num > b.lap_num) {
@@ -36,6 +51,10 @@ export default function LapTimes({ session_uid }) {
   const handleClick = (o) => {
     const { dataKey } = o;
     setShowDriver({ ...showDriver, [dataKey]: !showDriver[dataKey]});
+  }
+
+  const handleSliderChange = (event, newValue) => {
+    setDomain(newValue);
   }
 
   const timeScale = x => {
@@ -121,8 +140,40 @@ export default function LapTimes({ session_uid }) {
     }
   }, [drivers]);
 
+  useEffect(() => {
+    if (dataMax > -1 && dataMin > -1) {
+      setDomain([dataMin, dataMax]);
+    }
+  }, [dataMax, dataMin]);
+
+  useEffect(() => {
+    let newTicks = [];
+    for (let i = domain[0]; i <= domain[1]; i += 1000) {
+      newTicks.push(i);
+    }
+    setTicks(newTicks);
+    // if ((domain[1] - domain[0])/1000 + 1 !== numTicks) setNumTicks((domain[1] - domain[0])/1000 + 1);
+  }, [domain]);
+
+  useEffect(() => {
+    console.log(laps);
+  }, [laps]);
+
   return (
     <>
+      {
+        (dataMax > -1 && dataMin > -1) && (
+          <Slider 
+            value={domain}
+            onChange={handleSliderChange}
+            valueLabelDisplay="auto"
+            valueLabelFormat={timeScale}
+            min={dataMin}
+            max={dataMax}
+            step={1000}
+          />
+        )
+      }
       { laps.length > 0 && (
         <LineChart
           width={1000}
@@ -136,22 +187,26 @@ export default function LapTimes({ session_uid }) {
           data={laps}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="lap_num" type="number" tickCount={50} allowDecimals={false} domain={([dataMin, dataMax]) => [0, dataMax]} />
+          <XAxis dataKey="lap_num" type="number" tickCount={50} allowDecimals={false} domain={([min, max]) => [0, max]} />
           <YAxis 
-            domain={([dataMin, dataMax]) => { 
-              const minValue = Math.floor(dataMin/ 1000) * 1000;
-              const maxValue = Math.ceil(dataMax/1000) * 1000;
-              if ((maxValue - minValue)/1000 + 1 !== numTicks) setNumTicks((maxValue - minValue)/1000 + 1);
-              return [minValue, maxValue] 
+            domain={([min, max]) => { 
+              const minValue = Math.floor(min/1000) * 1000;
+              const maxValue = Math.ceil(max/1000) * 1000;
+              if (dataMax === -1 && isFinite(maxValue) && laps.length > 0) {
+                setDataMax(maxValue);
+              }
+              if (dataMin === -1 && isFinite(minValue) && laps.length > 0) setDataMin(minValue);
+              if ((maxValue - minValue)/1000 + 1 !== numTicks && laps.length > 0) setNumTicks((maxValue - minValue)/1000 + 1);
+              if (dataMax === -1 || dataMin === -1) {
+                return [minValue, maxValue];
+              }
+              return domain;
             }} 
             tickFormatter={timeScale}
             tickCount={numTicks}
           />
           <Tooltip content={<CustomToolTip />} />
           <Legend onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick} />
-          <Button>
-            Hi
-          </Button>
           { showDriver && drivers.map((d, i) => {
             return (
               <Line type="linear" dataKey={d} stroke={colors[d]} strokeWidth={3} strokeOpacity={opacity[d]} hide={showDriver[d]} activeDot={{ r: 8 }} connectNulls={false} />
